51f21f00696efde8840a104c2c88c2a211b6a566
kenei <kevinkenei9@gmail.com>
2020-11-10 18:14:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/places/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.places
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+const val TAG="MAIN ACTIVITY"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Initialize the SDK
+        Places.initialize(applicationContext, getString(R.string.apikey))
+
+        // Create a new PlacesClient instance
+        val placesClient = Places.createClient(this)
+        // Initialize the AutocompleteSupportFragment.
+        val autocompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment)
+                    as AutocompleteSupportFragment
+        // Specify the types of place data to return.
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: ${place.name}, ${place.id}")
+            }
+
+            override fun onError(p0: Status) {
+                TODO("Not yet implemented")
+                Log.i(TAG, "An error occurred: ")
+
+            }
+        })
+
+
+    }
+}
